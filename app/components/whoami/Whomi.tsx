4fb23e2927728939805ca3a23f0c7d6a3e5b4fba
Maryna <maryny.khrystyianyn@gmail.com>
2025-10-06 22:09:25 +0200

    resolvedThema

--- app/components/whoami/Whomi.tsx
+++ app/components/whoami/Whomi.tsx
@@ -1,31 +1,37 @@
 "use client";
 import { useTranslation } from "react-i18next";
 import { useTheme } from "next-themes";
-import TypeWriter from "../typeWriter/TypeWriter";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 const Whomi = () => {
   const { t } = useTranslation();
-  const { theme, setTheme } = useTheme();
-  
-  const imgUrl = theme === "dark" ? "/m.png" : "/m-light.png";
+  const { resolvedTheme } = useTheme(); // <== wichtig
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true); // true nach dem ersten Client-Render
+  }, []);
+
+  if (!mounted) {
+    // noch kein echtes Theme bekannt, also nichts anzeigen
+    return null;
+  }
+
+  const imgUrl = resolvedTheme === "dark" ? "/m.png" : "/m-light.png";
 
   return (
     <div>
       <h3>$ {t("whoami")}</h3>
       <div>
-        <h1 className="text-2xl sm:text-4xl font-bold flex w-[300px] sm:w-[400px]  items-end">
+        <h1 className="text-2xl sm:text-4xl font-bold flex w-[300px] sm:w-[400px] items-end">
           <Image src={imgUrl} width={50} height={50} alt="m" className="pb-1" />
-          <div className="">{t("name")}</div>
+          <div>{t("name")}</div>
         </h1>
 
         <div className="font-mono text-2xl tracking-widest text-[var(--muted)] py-1">
-        {t("webdev")}
+          {t("webdev")}
         </div>
-
-        {/* <div className="h-10 text-xl">
-            I am <TypeWriter text="a Web Developer" />
-          </div> */}
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -9,16 +9,23 @@ import Skills from "./components/skills/Skills";
 import Navigation from "./components/navigation/Navigation";
 
 export default function Home() {
-  const { theme, setTheme } = useTheme();
-  const [bgImgUrl, setBgImgUrl] = useState<string>("url('/bg-img.png')");
+  const { theme, setTheme, resolvedTheme } = useTheme();
+
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    if (theme === "dark") {
-      setBgImgUrl("url('/bg-img.png')");
-    } else {
-      setBgImgUrl("url('/bg-img-light2.png')");
-    }
-  }, [theme]);
+    setMounted(true); // true nach dem ersten Client-Render
+  }, []);
+
+  if (!mounted) {
+    // noch kein echtes Theme bekannt, also nichts anzeigen
+    return null;
+  }
+
+  const bgImgUrl =
+    resolvedTheme === "dark"
+      ? "url('/bg-img.png')"
+      : "url('/bg-img-light2.png')";
 
   return (
     <main
@@ -26,7 +33,7 @@ export default function Home() {
       className={`relative flex  items-center  justify-center pt-5 md:pt-15 h-screen  bg-repeat-x bg-top bg-[var(--bg)] text-[var(--text)] `}
     >
       <Navigation />
-      <div className="flex flex-col gap-5 md:gap-15 p-6 bg-[var(--bg-main)] m-3 rounded-md border border-sky-950 w-[90%] max-w-[900px]">
+      <div className="flex flex-col gap-5 md:gap-15 p-6 pb-15 bg-[var(--bg-main)] m-3 rounded-md border border-sky-950 w-[90%] max-w-[900px]">
         <Header />
         <Whomi />
         <Skills />
