fa01c793bd87210b67f92dbb58e451e201618e13
Maryna <maryny.khrystyianyn@gmail.com>
2025-10-08 23:48:17 +0200

    mobil contacts image

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -8,8 +8,14 @@ const Contacts = () => {
   const { t } = useTranslation();
   return (
     <div className="flex flex-col md:flex-row gap-5 justify-between items-center mt-[-20px]">
-      <div className="flex md:flex-col justify-center items-center gap-5 ">
-        <Image src="/frends.png" width={250} height={250} alt="girafe" />
+      <div className="flex md:flex-col justify-center items-center gap-15 sm:gap-5">
+        <div className="sm:block hidden">
+          <Image src="/frends.png" width={250} height={250} alt="frends" />
+        </div>
+
+        <div className="sm:hidden h-[180px] ">
+          <Image src="/frends.png" width={160} height={180} alt="frends" />
+        </div>
 
         <BtnSci />
       </div>
