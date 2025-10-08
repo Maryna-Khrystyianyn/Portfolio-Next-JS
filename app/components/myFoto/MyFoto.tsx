95679cdca37f82979acb9b216a8025701ae458b2
Maryna <maryny.khrystyianyn@gmail.com>
2025-10-08 16:11:36 +0200

    fix blink mit fono

--- app/components/myFoto/MyFoto.tsx
+++ app/components/myFoto/MyFoto.tsx
@@ -1,16 +1,27 @@
-import styles from './myFoto.module.css';
+import styles from "./myFoto.module.css";
+
+import { useState } from "react";
 import Image from "next/image";
 
-const MyFoto=()=> {
+function MyFoto() {
+  const [loaded, setLoaded] = useState(false);
+
   return (
     <div className="grid-area-foto flex justify-center items-center">
       <div className={styles.imgBox}>
         <div className={styles.imgItem}>
-          <Image src="/me4.jpg" width={300} height={300} alt="Maryna Khrystyianyn" className={styles.mainFotoImg} />
+          <Image
+            src="/me4.jpg"
+            width={300}
+            height={300}
+            alt="Maryna Khrystyianyn"
+            className={`${styles.mainFotoImg} ${loaded ? styles.loaded : ''}`}
+            onLoadingComplete={() => setLoaded(true)}
+          />
         </div>
       </div>
     </div>
   );
 }
 
-export default MyFoto
+export default MyFoto;
